84532e62d7f00329add95b76ce29b992bddf2086
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-08-22 01:44:49 +0300

    Add project files.

---
+++ TestWithExcell/ExcelTool.cs
@@ -0,0 +1,102 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace TestWithExcell
+{
+    public static class ExcelTool
+    {
+        public static void CreateFile()
+        {
+            //Create a new ExcelPackage
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                //Set some properties of the Excel document
+                excelPackage.Workbook.Properties.Author = "Andrey";
+                excelPackage.Workbook.Properties.Title = "Title of Document";
+                excelPackage.Workbook.Properties.Subject = "EPPlus demo export data";
+                excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                //Create the WorkSheet
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Sheet 1");
+
+                //Add some text to cell A1
+                worksheet.Cells["A1"].Value = "My first EPPlus spreadsheet!";
+                //You could also use [line, column] notation:
+                worksheet.Cells[1, 2].Value = "This is cell B1!";
+
+                //Save your file
+                FileInfo fi = new FileInfo(@"C:/adb/AndreyFile.xlsx");
+                excelPackage.SaveAs(fi);
+            }
+        }
+
+        public static DataTable CreateDataTable(ExcelWorksheet worksheet)
+        {
+            DataTable dt = new DataTable();
+
+
+            //check if the worksheet is completely empty
+            if (worksheet.Dimension == null)
+            {
+                return dt;
+            }
+
+            //create a list to hold the column names
+            List<string> columnNames = new List<string>();
+
+            //needed to keep track of empty column headers
+            int currentColumn = 1;
+
+            //loop all columns in the sheet and add them to the datatable
+            foreach (var cell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column])
+            {
+                string columnName = cell.Text.Trim();
+
+                //check if the previous header was empty and add it if it was
+                if (cell.Start.Column != currentColumn)
+                {
+                    columnNames.Add("Header_" + currentColumn);
+                    dt.Columns.Add("Header_" + currentColumn);
+                    currentColumn++;
+                }
+
+                //add the column name to the list to count the duplicates
+                columnNames.Add(columnName);
+
+                //count the duplicate column names and make them unique to avoid the exception
+                //A column named 'Name' already belongs to this DataTable
+                int occurrences = columnNames.Count(x => x.Equals(columnName));
+                if (occurrences > 1)
+                {
+                    columnName = columnName + "_" + occurrences;
+                }
+
+                //add the column to the datatable
+                dt.Columns.Add(columnName);
+
+                currentColumn++;
+            }
+
+            //start adding the contents of the excel file to the datatable
+            for (int i = 2; i <= worksheet.Dimension.End.Row; i++)
+            {
+                var row = worksheet.Cells[i, 1, i, worksheet.Dimension.End.Column];
+                DataRow newRow = dt.NewRow();
+
+                //loop all cells in the row
+                foreach (var cell in row)
+                {
+                    newRow[cell.Start.Column - 1] = cell.Text;
+                }
+
+                dt.Rows.Add(newRow);
+            }
+
+            return dt;
+        }
+    }
+}
---
+++ TestWithExcell/Program.cs
@@ -0,0 +1,94 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace TestWithExcell
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filePath = @"c:/adb/AndreyFile.xlsx";
+            var resultBag = new List<List<string>>();
+            var file = new FileInfo(filePath);
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                //create an instance of the the first sheet in the loaded file
+                ExcelWorksheet worksheet1 = excelPackage.Workbook.Worksheets[0];
+                var firstTable = ExcelTool.CreateDataTable(worksheet1);
+
+                var firstSheet = new List<object[]>();
+
+                foreach (var item in firstTable.Rows)
+                {
+                    var currentRow = (DataRow)item;
+                    var cells = currentRow.ItemArray;
+                    firstSheet.Add(cells);
+                }
+
+                ExcelWorksheet worksheet2 = excelPackage.Workbook.Worksheets[1];
+                var secondTable = ExcelTool.CreateDataTable(worksheet2);
+
+                var secondSheet = new List<object[]>();
+
+                foreach (var item in secondTable.Rows)
+                {
+                    var currentRow = (DataRow)item;
+                    var cells = currentRow.ItemArray;
+                    secondSheet.Add(cells);
+                }
+
+
+                foreach (var item1 in firstSheet)
+                {
+                    var platec1 = item1[0].ToString();
+                    var obekt = item1[1].ToString();
+
+
+                    foreach (var item2 in secondSheet)
+                    {
+                        var currentRow = new List<string>();
+                        var platec2 = item2[0].ToString();
+
+                        if (platec1.Equals(platec2))
+                        {
+                            var table = "Table";
+                            var product = item2[1].ToString();
+                            var days = item2[2].ToString();
+                            currentRow.Add(table);
+                            currentRow.Add(obekt);
+                            currentRow.Add(product);
+                            currentRow.Add(days);
+
+                            resultBag.Add(currentRow);
+                        }
+                    }
+                }
+            }
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet resultSheet = excelPackage.Workbook.Worksheets.Add("Result");
+                excelPackage.Save();
+            }
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet resultSheet = excelPackage.Workbook.Worksheets[2];
+
+                for (int i = 0; i < resultBag.Count; i++)
+                {
+                    var currentRow = resultBag[i];
+
+                    for (int j = 0; j < currentRow.Count; j++)
+                    {
+                        resultSheet.Cells[i + 1, j + 1].Value = currentRow[j];
+                    }
+                }
+                excelPackage.Save();
+            }
+        }
+    }
+}
